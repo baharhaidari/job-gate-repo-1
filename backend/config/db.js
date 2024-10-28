d99cd62c7fac3481c1f303b23ab87190e834e66f
Bahar Haidari <149867578+baharhaidari@users.noreply.github.com>
2024-10-28 15:42:33 +0430

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render’s managed PostgreSQL
+  },
 });
+
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   connectionString: process.env.DATABASE_URL,
+// });
 
 module.exports = pool;
