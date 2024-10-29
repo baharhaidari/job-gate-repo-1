e096ab9469aaafdaf85a15bce3c0611b528a0735
Bahar Haidari <149867578+baharhaidari@users.noreply.github.com>
2024-10-29 10:15:54 +0430

    Update jobController.js

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -39,7 +39,7 @@ const getAllJobs = async (req, res) => {
 const getJobById = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM jobs WHERE job_id = $1", [id]);
+    const result = await pool.query("SELECT * FROM jobs WHERE id = $1", [id]);
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "Job not found" });
     }
@@ -141,7 +141,7 @@ const deleteJob = async (req, res) => {
 
   try {
     const result = await pool.query(
-      "DELETE FROM jobs WHERE job_id = $1 RETURNING *",
+      "DELETE FROM jobs WHERE id = $1 RETURNING *",
       [id]
     );
 
